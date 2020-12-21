bcc741ebda06cca7fa0281bdacc51a8fcfbe8a4d
Clara Pasteau <clarapasteau@gmail.com>
2020-12-21 22:08:27 +0100

    site

--- sketch.js
+++ sketch.js
@@ -35,3 +35,29 @@ window.addEventListener("load", function load() {
       document.getElementById(tabName).style.display = "block";
       evt.currentTarget.className += " active";
     }
+
+
+    function checkTime(i) {
+      if (i < 10) {
+        i = "0" + i;
+      }
+      return i;
+    }
+    
+    function startTime() {
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      // add a zero in front of numbers<10
+      m = checkTime(m);
+      s = checkTime(s);
+      document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+      t = setTimeout(function() {
+        startTime()
+      }, 500);
+    }
+    startTime();
+
+    var dt = new Date();
+document.getElementById("date").innerHTML = dt.toLocaleDateString();
